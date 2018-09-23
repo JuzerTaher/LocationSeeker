cee5771909906360533785f2e5413e2ab7808e68
JuzerTaher <juzerthehero@gmail.com>
2018-09-23 21:11:19 +0530

    Now, we can trace path from current location to our college-AVANTHI.

--- app/src/main/java/com/example/juzer/linking1and2/MapsActivity.java
+++ app/src/main/java/com/example/juzer/linking1and2/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.juzer.linking1and2;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,12 +13,28 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     double lat=MainActivity.latitude;
     double lon=MainActivity.longitude;
+
+    double avLat=17.942584,avLon=83.421228;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +61,160 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
-        // Add a marker in current Location and move the camera
+        // Add a marker in current Location and avanthi location
         LatLng currentLocation = new LatLng(lat, lon);
+        LatLng avanthi=new LatLng(avLat,avLon);
+
+        //Avanthi Marker
+        mMap.addMarker(new MarkerOptions().position(avanthi).title("Avanthi College")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,17));
+
+        //Present location marker
         mMap.addMarker(new MarkerOptions().position(currentLocation).title("Your Location")
         .icon(BitmapDescriptorFactory.fromResource(R.drawable.bikemapfinal)));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,17));
+
+
+        String url=getRequestUrl(currentLocation,avanthi);
+        TaskRequestDirections taskRequestDirections=new TaskRequestDirections();
+        taskRequestDirections.execute(url);
+
+
+        /*code for showing path between two points...
+        ArrayList points = null;
+        PolylineOptions lineOptions = null;
+        points = new ArrayList();
+        lineOptions = new PolylineOptions();
+
+        points.add(currentLocation);
+        points.add(avanthi);
+
+        lineOptions.addAll(points);
+        lineOptions.width(12);
+        lineOptions.color(Color.BLACK);
+        lineOptions.geodesic(true);
+
+        mMap.addPolyline(lineOptions);*/
+    }
+
+    private String getRequestUrl(LatLng origin, LatLng destination) {
+
+        String org="origin="+origin.latitude+","+origin.longitude;
+        String dest="destination="+destination.latitude+","+destination.longitude;
+        String sensor="sensor=false";
+        String mode="mode=driving";
+        String param=org+"&"+dest+"&"+sensor+"&"+mode;
+        String output="json";
+        String url="https://maps.googleapis.com/maps/api/directions/"+output+"?"+param;
+        return url;
+
+
+    }
+
+    private String requestDirection(String reqUrl) throws IOException {
+        String responseString = "";
+        InputStream inputStream = null;
+        HttpURLConnection httpURLConnection = null;
+        try{
+            URL url = new URL(reqUrl);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.connect();
+
+            //Get the response result
+            inputStream = httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+
+            responseString = stringBuffer.toString();
+            bufferedReader.close();
+            inputStreamReader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            httpURLConnection.disconnect();
+        }
+        return responseString;
+    }
+    public class TaskRequestDirections extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String responseString = "";
+            try {
+                responseString = requestDirection(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return  responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //Parse json here
+            TaskParser taskParser = new TaskParser();
+            taskParser.execute(s);
+        }
+    }
+
+    public class TaskParser extends AsyncTask<String, Void, List<List<HashMap<String, String>>> > {
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... strings) {
+            JSONObject jsonObject = null;
+            List<List<HashMap<String, String>>> routes = null;
+            try {
+                jsonObject = new JSONObject(strings[0]);
+                DirectionParser directionParser = new DirectionParser();
+                routes = directionParser.parse(jsonObject);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> lists) {
+            //Get list route and display it into the map
+
+            ArrayList points = null;
+
+            PolylineOptions polylineOptions = null;
+
+            for (List<HashMap<String, String>> path : lists) {
+                points = new ArrayList();
+                polylineOptions = new PolylineOptions();
+
+                for (HashMap<String, String> point : path) {
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lon = Double.parseDouble(point.get("lon"));
+
+                    points.add(new LatLng(lat,lon));
+                }
+
+                polylineOptions.addAll(points);
+                polylineOptions.width(15);
+                polylineOptions.color(Color.BLUE);
+                polylineOptions.geodesic(true);
+            }
+
+            if (polylineOptions!=null) {
+                mMap.addPolyline(polylineOptions);
+            } else {
+                Toast.makeText(getApplicationContext(), "Direction not found!", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 }
